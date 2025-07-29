034d672c1ee58cf4a9000813b10a65c0cc7d7e4b
UselessJoy <mrqwer2014@mail.ru>
2025-07-29 15:55:32 +0300

    Update pkglist-redos.sh

--- tools/libs/pkglist-redos.sh
+++ tools/libs/pkglist-redos.sh
@@ -2,6 +2,6 @@
 
 set -Ee
 # crudini
-PKGLIST="git util-linux findutils v4l-utils curl"
+PKGLIST="git crudini util-linux findutils v4l-utils curl"
 ### Ustreamer Dependencies
 PKGLIST="${PKGLIST} gcc gcc-c++ make libevent-devel libjpeg-turbo-devel libbsd-devel pkgconf"
